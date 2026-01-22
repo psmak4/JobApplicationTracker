4a18ad37894c0412ac08f33e1bea08a3b39ec0cb
Preston Jones <psmak4@gmail.com>
2026-01-22 16:58:28 -0500

    Rename component

--- ui/src/pages/Dashboard.tsx
+++ ui/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { ArrowUpDown, Cog, Filter as FilterIcon, LayoutGrid, Plus, Table as TableIcon, X } from 'lucide-react'
 import { useEffect, useMemo, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import ApplicationStatusBadge from '@/components/ApplicationBadge'
+import ApplicationStatusBadge from '@/components/ApplicationStatusBadge'
 import { Button, buttonVariants } from '../components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card'
 import { Checkbox } from '../components/ui/checkbox'
