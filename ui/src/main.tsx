7718229439bcde43a82fb73142a9fcd3ec5bd778
Preston Jones <psmak4@gmail.com>
2026-02-22 21:52:04 -0500

    refactor: update Sentry import path to `lib/sentry.ts`.

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './sentry'
+import './lib/sentry.ts'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
